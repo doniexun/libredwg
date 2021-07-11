PABLOCORRO/libredwg_clone_windows

Enable Boehm-Weiser libgc, if found and not disabled
via --disable-gc
should be much faster than our free mess. also leak-free and no
free errors.
even without free (programs with just one arg), it is faster.

however with large files its attrocious:

time programs/dwg2dxf --force-free -v0 -y ../test/test-big/2007/*.dwg
Reading DWG file ../test/test-big/2007/big.dwg
GC Warning: Repeated allocation of very large block (appr. size 37613568):
	May lead to memory leak and poor performance
GC Warning: Repeated allocation of very large block (appr. size 45481984):
	May lead to memory leak and poor performance
GC Warning: Repeated allocation of very large block (appr. size 45891584):
	May lead to memory leak and poor performance
GC Warning: Repeated allocation of very large block (appr. size 57073664):
	May lead to memory leak and poor performance
Writing DXF file big.dxf
Reading DWG file ../test/test-big/2007/HARTA_E_PRISHTINES.dwg
Terminated

real	12m19.230s
user	0m57.741s
sys	0m17.788s

vs --disable-gc
time programs/dwg2dxf --force-free -v0 -y ../test/test-big/2007/*.dwg
Reading DWG file ../test/test-big/2007/big.dwg
Writing DXF file big.dxf
Reading DWG file ../test/test-big/2007/HARTA_E_PRISHTINES.dwg
Writing DXF file HARTA_E_PRISHTINES.dxf

real	0m25.661s
user	0m21.394s
sys	0m1.109s

src/bits.c
@@ -1444,7 +1444,7 @@ bit_read_fixed (Bit_Chain *restrict dat, BITCODE_RC *restrict dest,
 }
 
 /** Read fixed text with zero-termination.
- *  After usage, the allocated memory must be properly freed.
+ *  After usage, the allocated memory must be properly FREEd.
  *  preR11
  */
 ATTRIBUTE_MALLOC
@@ -1453,7 +1453,7 @@ bit_read_TF (Bit_Chain *restrict dat, unsigned int length)
 {
   BITCODE_RC *chain;
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (BITCODE_RC *)calloc (length + 1, 1);
+  chain = (BITCODE_RC *)CALLOC (length + 1, 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1467,7 +1467,7 @@ bit_read_TF (Bit_Chain *restrict dat, unsigned int length)
 }
 
 /** Read fixed text with zero-termination.
- *  After usage, the allocated memory must be properly freed.
+ *  After usage, the allocated memory must be properly FREEd.
  *  preR11
  */
 ATTRIBUTE_MALLOC
@@ -1478,7 +1478,7 @@ bit_read_bits (Bit_Chain *dat, unsigned long bits)
   int rest = bits % 8;
   BITCODE_RC *restrict chain;
   CHK_OVERFLOW_PLUS (bytes,__FUNCTION__,NULL)
-  chain = (BITCODE_RC *)calloc (bytes + (rest ? 2 : 1), 1);
+  chain = (BITCODE_RC *)CALLOC (bytes + (rest ? 2 : 1), 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1532,7 +1532,7 @@ bit_write_TF (Bit_Chain *restrict dat, BITCODE_TF restrict chain, unsigned int l
     }
 }
 
-/** Read simple text. After usage, the allocated memory must be properly freed.
+/** Read simple text. After usage, the allocated memory must be properly FREEd.
  */
 BITCODE_TV
 bit_read_TV (Bit_Chain *restrict dat)
@@ -1544,7 +1544,7 @@ bit_read_TV (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (unsigned char *)malloc (length + 1);
+  chain = (unsigned char *)MALLOC (length + 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1585,7 +1585,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
   if (!wstr)
     return NULL;
   size = len + 1;
-  str = (char*)malloc (size);
+  str = (char*)MALLOC (size);
   if (!str)
     return NULL;
   read = write = 0;
@@ -1608,7 +1608,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
           if (write + 1 >= size) // TODO should not happen
             {
               size += 2;
-              str = (char*)realloc (str, size);
+              str = (char*)REALLOC (str, size);
             }
           str[write++] = c & 0xFF;
         }
@@ -1617,7 +1617,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
           if (write + 7 > size)
             {
               size += 8;
-              str = (char*)realloc (str, size);
+              str = (char*)REALLOC (str, size);
             }
           str[write++] = '\\';
           str[write++] = 'U';
@@ -1862,7 +1862,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
               bit_write_BS (dat, length);
               for (i = 0; i < length; i++)
                 bit_write_RC (dat, (unsigned char)str[i]);
-              if (str) free (str);
+              if (str) FREE (str);
             }
         }
       else
@@ -1882,7 +1882,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
           else
             {
               uint16_t c;
-              BITCODE_TU ws = (BITCODE_TU)malloc ((strlen (s) + 1) * 2);
+              BITCODE_TU ws = (BITCODE_TU)MALLOC ((strlen (s) + 1) * 2);
               BITCODE_TU orig = ws;
               while ((c = *s++))
                 {
@@ -1903,7 +1903,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
                 }
               *ws = 0;
               bit_write_TU (dat, orig);
-              free (orig);
+              FREE (orig);
             }
         }
       else
@@ -1924,7 +1924,7 @@ bit_read_TU (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1949,7 +1949,7 @@ bit_read_TU_len (Bit_Chain *restrict dat, unsigned int *lenp)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1976,7 +1976,7 @@ bit_read_T16 (Bit_Chain *restrict dat)
   CHK_OVERFLOW(__FUNCTION__,NULL)
   length = bit_read_RS (dat);
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (BITCODE_TV)malloc (length + 1);
+  chain = (BITCODE_TV)MALLOC (length + 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2000,7 +2000,7 @@ bit_read_TU16 (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (2,__FUNCTION__,NULL)
   length = bit_read_RS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2033,7 +2033,7 @@ bit_read_T32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      wstr = (BITCODE_TU)malloc (size + 2);
+      wstr = (BITCODE_TU)MALLOC (size + 2);
       if (!wstr)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2055,7 +2055,7 @@ bit_read_T32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      str = (BITCODE_T32)malloc (size + 1);
+      str = (BITCODE_T32)MALLOC (size + 1);
       if (!str)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2090,7 +2090,7 @@ bit_read_TU32 (Bit_Chain *restrict dat)
                      dat->byte, size)
           return NULL;
         }
-      wstr = (BITCODE_TU)malloc (size + 2);
+      wstr = (BITCODE_TU)MALLOC (size + 2);
       if (!wstr)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2129,7 +2129,7 @@ bit_read_TU32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      str = (BITCODE_T32)malloc (size + 1);
+      str = (BITCODE_T32)MALLOC (size + 1);
       if (!str)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2241,7 +2241,7 @@ bit_read_T (Bit_Chain *restrict dat)
 }
 
 /* converts UCS-2LE to UTF-8.
-   first pass to get the dest len. single malloc.
+   first pass to get the dest len. single MALLOC.
  */
 char *
 bit_convert_TU (const BITCODE_TU restrict wstr)
@@ -2288,7 +2288,7 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
         LOG_INSANE ("U+%04X ", c);
 #endif
     }
-  str = (char*)malloc (len + 1);
+  str = (char*)MALLOC (len + 1);
   if (!str)
     {
       loglevel = 1;
@@ -2341,7 +2341,7 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
       else /* if (c < 0x10000) */
         { /* windows ucs-2 has no D800-DC00 surrogate pairs. go straight up */
           /*if (i+3 > len) {
-            str = realloc(str, i+3);
+            str = REALLOC(str, i+3);
             len = i+2;
           }*/
           str[i++] = (c >> 12) | 0xE0;
@@ -2367,10 +2367,10 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
 
 #define EXTEND_SIZE(str, i, len)                \
   if (i > len)                                  \
-    str = (char*) realloc (str, i+1)
+    str = (char*) REALLOC (str, i+1)
 
 /* converts UCS-2LE to UTF-8. len is the wstr length, not the resulting utf8-size.
-   single pass with realloc. */
+   single pass with REALLOC. */
 char *
 bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
 {
@@ -2381,7 +2381,7 @@ bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
 
   if (!wstr || !len)
     return NULL;
-  str = (char*)malloc (len + 1);
+  str = (char*)MALLOC (len + 1);
   if (!str)
     {
       loglevel = 1;
@@ -2437,7 +2437,7 @@ bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
       else /* if (c < 0x10000) */
         { /* windows ucs-2 has no D800-DC00 surrogate pairs. go straight up */
           /*if (i+3 > len) {
-            str = realloc(str, i+3);
+            str = REALLOC(str, i+3);
             len = i+2;
           }*/
           EXTEND_SIZE(str, i + 2, len);
@@ -2584,7 +2584,7 @@ bit_utf8_to_TU (char *restrict str, const unsigned cquoted)
   int len = strlen (str);
   unsigned char c;
 
-  wstr = (BITCODE_TU)malloc (2 * (len + 1));
+  wstr = (BITCODE_TU)MALLOC (2 * (len + 1));
   if (!wstr)
     {
       loglevel = 1;
@@ -2653,7 +2653,7 @@ bit_eq_TU (const char *restrict str, BITCODE_TU restrict wstr)
     return (wstr && *wstr) ? 0 : 1;
   utf8 = bit_convert_TU (wstr);
   result = utf8 ? (strcmp (str, utf8) ? 0 : 1) : 0;
-  free (utf8);
+  FREE (utf8);
   return result;
 }
 
@@ -2685,7 +2685,7 @@ BITCODE_T
 bit_set_T (Bit_Chain *dat, const char* restrict src)
 {
   if (!(IS_FROM_TU (dat)))
-    return strdup (src);
+    return STRDUP (src);
   else
     return (BITCODE_T)bit_utf8_to_TU ((char*)src, 0);
 }
@@ -2905,7 +2905,7 @@ bit_read_ENC (Bit_Chain *dat, Bit_Chain *hdl_dat, Bit_Chain *str_dat,
         color->rgb = bit_read_BL (dat); // ODA bug, documented as BS
       if (flag & 0x40)
         {
-          color->handle = (BITCODE_H)calloc (1, sizeof (Dwg_Object_Ref));
+          color->handle = (BITCODE_H)CALLOC (1, sizeof (Dwg_Object_Ref));
           if (!color->handle)
             {
               loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2988,7 +2988,7 @@ bit_write_sentinel (Bit_Chain *dat, unsigned char sentinel[16])
 void
 bit_chain_init (Bit_Chain *dat, const int size)
 {
-  dat->chain = (unsigned char *)calloc (1, size);
+  dat->chain = (unsigned char *)CALLOC (1, size);
   if (!dat->chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -3023,7 +3023,7 @@ bit_chain_alloc (Bit_Chain *dat)
   else
     {
       dat->chain
-          = (unsigned char *)realloc (dat->chain, dat->size + CHAIN_BLOCK);
+          = (unsigned char *)REALLOC (dat->chain, dat->size + CHAIN_BLOCK);
       if (!dat->chain)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -3038,11 +3038,7 @@ bit_chain_alloc (Bit_Chain *dat)
 void
 bit_chain_free (Bit_Chain *dat)
 {
-  if (dat->chain)
-    {
-      free (dat->chain);
-      dat->chain = NULL;
-    }
+  FREE_IF (dat->chain);
   dat->size = 0;
 }
 

doniexun/libredwg

src/bits.c
@@ -1444,7 +1444,7 @@ bit_read_fixed (Bit_Chain *restrict dat, BITCODE_RC *restrict dest,
 }
 
 /** Read fixed text with zero-termination.
- *  After usage, the allocated memory must be properly freed.
+ *  After usage, the allocated memory must be properly FREEd.
  *  preR11
  */
 ATTRIBUTE_MALLOC
@@ -1453,7 +1453,7 @@ bit_read_TF (Bit_Chain *restrict dat, unsigned int length)
 {
   BITCODE_RC *chain;
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (BITCODE_RC *)calloc (length + 1, 1);
+  chain = (BITCODE_RC *)CALLOC (length + 1, 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1467,7 +1467,7 @@ bit_read_TF (Bit_Chain *restrict dat, unsigned int length)
 }
 
 /** Read fixed text with zero-termination.
- *  After usage, the allocated memory must be properly freed.
+ *  After usage, the allocated memory must be properly FREEd.
  *  preR11
  */
 ATTRIBUTE_MALLOC
@@ -1478,7 +1478,7 @@ bit_read_bits (Bit_Chain *dat, unsigned long bits)
   int rest = bits % 8;
   BITCODE_RC *restrict chain;
   CHK_OVERFLOW_PLUS (bytes,__FUNCTION__,NULL)
-  chain = (BITCODE_RC *)calloc (bytes + (rest ? 2 : 1), 1);
+  chain = (BITCODE_RC *)CALLOC (bytes + (rest ? 2 : 1), 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1532,7 +1532,7 @@ bit_write_TF (Bit_Chain *restrict dat, BITCODE_TF restrict chain, unsigned int l
     }
 }
 
-/** Read simple text. After usage, the allocated memory must be properly freed.
+/** Read simple text. After usage, the allocated memory must be properly FREEd.
  */
 BITCODE_TV
 bit_read_TV (Bit_Chain *restrict dat)
@@ -1544,7 +1544,7 @@ bit_read_TV (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (unsigned char *)malloc (length + 1);
+  chain = (unsigned char *)MALLOC (length + 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1585,7 +1585,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
   if (!wstr)
     return NULL;
   size = len + 1;
-  str = (char*)malloc (size);
+  str = (char*)MALLOC (size);
   if (!str)
     return NULL;
   read = write = 0;
@@ -1608,7 +1608,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
           if (write + 1 >= size) // TODO should not happen
             {
               size += 2;
-              str = (char*)realloc (str, size);
+              str = (char*)REALLOC (str, size);
             }
           str[write++] = c & 0xFF;
         }
@@ -1617,7 +1617,7 @@ bit_embed_TU_size (BITCODE_TU restrict wstr, const int len)
           if (write + 7 > size)
             {
               size += 8;
-              str = (char*)realloc (str, size);
+              str = (char*)REALLOC (str, size);
             }
           str[write++] = '\\';
           str[write++] = 'U';
@@ -1862,7 +1862,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
               bit_write_BS (dat, length);
               for (i = 0; i < length; i++)
                 bit_write_RC (dat, (unsigned char)str[i]);
-              if (str) free (str);
+              if (str) FREE (str);
             }
         }
       else
@@ -1882,7 +1882,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
           else
             {
               uint16_t c;
-              BITCODE_TU ws = (BITCODE_TU)malloc ((strlen (s) + 1) * 2);
+              BITCODE_TU ws = (BITCODE_TU)MALLOC ((strlen (s) + 1) * 2);
               BITCODE_TU orig = ws;
               while ((c = *s++))
                 {
@@ -1903,7 +1903,7 @@ bit_write_T (Bit_Chain *restrict dat, BITCODE_T restrict s)
                 }
               *ws = 0;
               bit_write_TU (dat, orig);
-              free (orig);
+              FREE (orig);
             }
         }
       else
@@ -1924,7 +1924,7 @@ bit_read_TU (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1949,7 +1949,7 @@ bit_read_TU_len (Bit_Chain *restrict dat, unsigned int *lenp)
   CHK_OVERFLOW_PLUS (1,__FUNCTION__,NULL)
   length = bit_read_BS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -1976,7 +1976,7 @@ bit_read_T16 (Bit_Chain *restrict dat)
   CHK_OVERFLOW(__FUNCTION__,NULL)
   length = bit_read_RS (dat);
   CHK_OVERFLOW_PLUS (length,__FUNCTION__,NULL)
-  chain = (BITCODE_TV)malloc (length + 1);
+  chain = (BITCODE_TV)MALLOC (length + 1);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2000,7 +2000,7 @@ bit_read_TU16 (Bit_Chain *restrict dat)
   CHK_OVERFLOW_PLUS (2,__FUNCTION__,NULL)
   length = bit_read_RS (dat);
   CHK_OVERFLOW_PLUS (length * 2,__FUNCTION__,NULL)
-  chain = (BITCODE_TU)malloc ((length + 1) * 2);
+  chain = (BITCODE_TU)MALLOC ((length + 1) * 2);
   if (!chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2033,7 +2033,7 @@ bit_read_T32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      wstr = (BITCODE_TU)malloc (size + 2);
+      wstr = (BITCODE_TU)MALLOC (size + 2);
       if (!wstr)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2055,7 +2055,7 @@ bit_read_T32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      str = (BITCODE_T32)malloc (size + 1);
+      str = (BITCODE_T32)MALLOC (size + 1);
       if (!str)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2090,7 +2090,7 @@ bit_read_TU32 (Bit_Chain *restrict dat)
                      dat->byte, size)
           return NULL;
         }
-      wstr = (BITCODE_TU)malloc (size + 2);
+      wstr = (BITCODE_TU)MALLOC (size + 2);
       if (!wstr)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2129,7 +2129,7 @@ bit_read_TU32 (Bit_Chain *restrict dat)
                      dat->byte, size)
             return NULL;
         }
-      str = (BITCODE_T32)malloc (size + 1);
+      str = (BITCODE_T32)MALLOC (size + 1);
       if (!str)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2241,7 +2241,7 @@ bit_read_T (Bit_Chain *restrict dat)
 }
 
 /* converts UCS-2LE to UTF-8.
-   first pass to get the dest len. single malloc.
+   first pass to get the dest len. single MALLOC.
  */
 char *
 bit_convert_TU (const BITCODE_TU restrict wstr)
@@ -2288,7 +2288,7 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
         LOG_INSANE ("U+%04X ", c);
 #endif
     }
-  str = (char*)malloc (len + 1);
+  str = (char*)MALLOC (len + 1);
   if (!str)
     {
       loglevel = 1;
@@ -2341,7 +2341,7 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
       else /* if (c < 0x10000) */
         { /* windows ucs-2 has no D800-DC00 surrogate pairs. go straight up */
           /*if (i+3 > len) {
-            str = realloc(str, i+3);
+            str = REALLOC(str, i+3);
             len = i+2;
           }*/
           str[i++] = (c >> 12) | 0xE0;
@@ -2367,10 +2367,10 @@ bit_convert_TU (const BITCODE_TU restrict wstr)
 
 #define EXTEND_SIZE(str, i, len)                \
   if (i > len)                                  \
-    str = (char*) realloc (str, i+1)
+    str = (char*) REALLOC (str, i+1)
 
 /* converts UCS-2LE to UTF-8. len is the wstr length, not the resulting utf8-size.
-   single pass with realloc. */
+   single pass with REALLOC. */
 char *
 bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
 {
@@ -2381,7 +2381,7 @@ bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
 
   if (!wstr || !len)
     return NULL;
-  str = (char*)malloc (len + 1);
+  str = (char*)MALLOC (len + 1);
   if (!str)
     {
       loglevel = 1;
@@ -2437,7 +2437,7 @@ bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
       else /* if (c < 0x10000) */
         { /* windows ucs-2 has no D800-DC00 surrogate pairs. go straight up */
           /*if (i+3 > len) {
-            str = realloc(str, i+3);
+            str = REALLOC(str, i+3);
             len = i+2;
           }*/
           EXTEND_SIZE(str, i + 2, len);
@@ -2584,7 +2584,7 @@ bit_utf8_to_TU (char *restrict str, const unsigned cquoted)
   int len = strlen (str);
   unsigned char c;
 
-  wstr = (BITCODE_TU)malloc (2 * (len + 1));
+  wstr = (BITCODE_TU)MALLOC (2 * (len + 1));
   if (!wstr)
     {
       loglevel = 1;
@@ -2655,7 +2655,7 @@ bit_eq_TU (const char *restrict str, BITCODE_TU restrict wstr)
     return (wstr && *wstr) ? 0 : 1;
   utf8 = bit_convert_TU (wstr);
   result = utf8 ? (strcmp (str, utf8) ? 0 : 1) : 0;
-  free (utf8);
+  FREE (utf8);
   return result;
 }
 
@@ -2687,7 +2687,7 @@ BITCODE_T
 bit_set_T (Bit_Chain *dat, const char* restrict src)
 {
   if (!(IS_FROM_TU (dat)))
-    return strdup (src);
+    return STRDUP (src);
   else
     return (BITCODE_T)bit_utf8_to_TU ((char*)src, 0);
 }
@@ -2907,7 +2907,7 @@ bit_read_ENC (Bit_Chain *dat, Bit_Chain *hdl_dat, Bit_Chain *str_dat,
         color->rgb = bit_read_BL (dat); // ODA bug, documented as BS
       if (flag & 0x40)
         {
-          color->handle = (BITCODE_H)calloc (1, sizeof (Dwg_Object_Ref));
+          color->handle = (BITCODE_H)CALLOC (1, sizeof (Dwg_Object_Ref));
           if (!color->handle)
             {
               loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -2990,7 +2990,7 @@ bit_write_sentinel (Bit_Chain *dat, unsigned char sentinel[16])
 void
 bit_chain_init (Bit_Chain *dat, const int size)
 {
-  dat->chain = (unsigned char *)calloc (1, size);
+  dat->chain = (unsigned char *)CALLOC (1, size);
   if (!dat->chain)
     {
       loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -3023,7 +3023,7 @@ void bit_chain_alloc_size (Bit_Chain *dat, const size_t size)
   else
     {
       dat->chain
-          = (unsigned char *)realloc (dat->chain, dat->size + size);
+          = (unsigned char *)REALLOC (dat->chain, dat->size + size);
       if (!dat->chain)
         {
           loglevel = dat->opts & DWG_OPTS_LOGLEVEL;
@@ -3045,11 +3045,7 @@ bit_chain_alloc (Bit_Chain *dat)
 void
 bit_chain_free (Bit_Chain *dat)
 {
-  if (dat->chain)
-    {
-      free (dat->chain);
-      dat->chain = NULL;
-    }
+  FREE_IF (dat->chain);
   dat->size = 0;
 }
 